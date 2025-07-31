fd8cdd9fce51722f9e958c54ffb5cf72a2819b05
Shahzaib-S110 <shahzaib110cci@gmail.com>
2025-07-31 16:54:14 +0500

    Fix: Add type for children in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,9 @@
-import './globals.css'
+import { ReactNode } from 'react';
 
-export const metadata = {
-  title: 'MindMuse',
-  description: 'Built with Next.js + Tailwind',
-}
-
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
